292544cbbbe5ef30b44364176eb651cfd6d19d47
abufghhiillll <49645398+abufghhiillll@users.noreply.github.com>
2019-04-16 16:18:33 +0530

    Create Discord.py

---
+++ Discord.py
@@ -0,0 +1,13 @@
+	$ git clone https://github.com/FastestMolasses/HQ-Answers.git
+$ cd HQ-Answers
+$ pip install -r requirements.txt
+$ touch config.py
+```
+
+ Your `config.py` file should look like this. Enter the relevant information.
+
+ ```
+DISCORD_TOKEN = 'NTQ1OTU5NDA3ODUxMjc0Mjcw.XLWytg.Kj1-CcNdXBox05S5CifjUDLgIaE'
+
+CHANNEL_ID = 000000 # Should be the channel you want the bot to post in. Leave as int.
+
